190e0aad56545cc58b004dfcf3aaffcd1511b709
feasible.creative <feasible.creative@gmail.com>
2022-08-08 19:34:14 -0400

    Add pseudocode and basic javascript to be debugged

---
+++ script.js
@@ -0,0 +1,38 @@
+// function getComputerChoice(){
+//     let Computer = 
+
+
+// }
+
+// playerSelection
+// computerSelection
+
+
+//1. Generate random input from Computer
+//2. Receive input from player
+//3. Determine winner of Game
+//4. Output outcome
+
+
+function yourChoice(){
+    let playerSelection = prompt("Your choice:");
+
+    
+    console.log(playerSelection);
+}
+
+yourChoice();
+
+let computerSelection = "rock";
+
+
+function playRound(){
+if playerSelection === computerSelection(
+    console.log("DRAW"))};
+   
+    
+// } else if{
+//     playerSelection === "Rock" && computerSelection === "Paper"{console.log("You Lose.")
+
+//     }
+// };
