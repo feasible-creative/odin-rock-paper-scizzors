d1bb2072ee51b49998382382f58a004c4d917c89
feasible.creative <feasible.creative@gmail.com>
2022-08-08 19:43:26 -0400

    create playRound function to output outcome of game

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//1. Generate random input from Computer
+//2. Receive input from player
+//3. Determine winner of Game
+//4. Output outcome
+
+
+
 // function getComputerChoice(){
 //     let Computer = 
 
@@ -8,28 +15,40 @@
 // computerSelection
 
 
-//1. Generate random input from Computer
-//2. Receive input from player
-//3. Determine winner of Game
-//4. Output outcome
 
 
 function yourChoice(){
-    let playerSelection = prompt("Your choice:");
-
-    
-    console.log(playerSelection);
+    return playerSelection = prompt("Your choice:");
+ 
 }
 
 yourChoice();
 
-let computerSelection = "rock";
+
+let computerSelection = "Scizzors";
 
 
 function playRound(){
-if playerSelection === computerSelection(
-    console.log("DRAW"))};
+if (playerSelection === computerSelection){
+    console.log("DRAW")
+} else if (playerSelection === "rock" && computerSelection === "paper"){
+    console.log("You Lose.");
+} else if (playerSelection === "paper" && computerSelection === "Scizzors"){
+    console.log("You Lose.");
+} else{
+    console.log("You Win!");
+}
+};
    
+    playRound()
+
+
+
+
+
+
+
+
     
 // } else if{
 //     playerSelection === "Rock" && computerSelection === "Paper"{console.log("You Lose.")
