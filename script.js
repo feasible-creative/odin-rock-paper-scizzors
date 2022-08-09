b5a9ca3fa522f9a163da624d3ea75a0408f76a94
feasible.creative <feasible.creative@gmail.com>
2022-08-09 08:52:08 -0400

    create start of nested function for player selection

--- script.js
+++ script.js
@@ -4,43 +4,17 @@
 //4. Output outcome
 
 
+    
+    
+
+
 
 // function getComputerChoice(){
-//     let Computer = 
-
+//  ['rock', 'paper', 'scizzors'];
+ 
 
 // }
 
-// playerSelection
-// computerSelection
-
-
-
-
-function yourChoice(){
-    return playerSelection = prompt("Your choice:");
- 
-}
-
-yourChoice();
-
-
-let computerSelection = "Scizzors";
-
-
-function playRound(){
-if (playerSelection === computerSelection){
-    console.log("DRAW")
-} else if (playerSelection === "rock" && computerSelection === "paper"){
-    console.log("You Lose.");
-} else if (playerSelection === "paper" && computerSelection === "Scizzors"){
-    console.log("You Lose.");
-} else{
-    console.log("You Win!");
-}
-};
-   
-    playRound()
 
 
 
@@ -48,10 +22,33 @@ if (playerSelection === computerSelection){
 
 
 
+// function yourChoice(){
+//     return answer = prompt("Your choice:")
+  
+// }
+
+
+
+
+function playRound(playerSelection, computerSelection){ 
+    function yourChoice(){
+        return playerSelection = prompt("Your choice:");}
+          yourChoice();
+    
+      
+
+
+    if (playerSelection === computerSelection){
+        console.log("DRAW")
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You Lose.");
+    } else if (playerSelection === "paper" && computerSelection === "scizzors"){
+        console.log("You Lose.");
+    } else{
+        console.log("You Win!");
+    }
 
     
-// } else if{
-//     playerSelection === "Rock" && computerSelection === "Paper"{console.log("You Lose.")
-
-//     }
-// };
+    };
+       
+playRound()
