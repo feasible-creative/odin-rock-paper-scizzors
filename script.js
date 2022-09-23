12060b4d8fff3e551ffda244ce9d594ef2292b54
feasible <feasible.creative@gmail.com>
2022-09-23 11:31:35 -0400

    Develop score tracker for winner of each round. Once either the player or computer earns 5 'wins' - display winner

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 //3. Determine winner of Game
 //4. Output outcome
 
-
 // Below to enter computer choice to HTML
 // const computer = document.querySelector('#computerOption');
 // const text = document.createElement('p');
@@ -11,90 +10,102 @@
 
 // computer.appendChild(text);
 
+const options = ["rock", "paper", "scizzors"];
+
+
+const playerScore = document.createElement('p')
+playerScore.textContent = 0;
+
+const PlayerScoreContainer = document.getElementById('playerScore').appendChild(playerScore);
+
+
+const computerScore = document.createElement('p')
+computerScore.textContent = 0;
+const computerScoreContainer = document.getElementById('computerScore').appendChild(computerScore);
 
 
 
-const rock = document.querySelector('#rock');
-const options = ['rock', 'paper', 'scizzors'];
-
-
-function game(playerSelection, computerSelection){
 
 
 
-const button = document.querySelectorAll('.option');
+function playRound(playerSelection, computerSelection) {
+  const button = document.querySelectorAll(".option");
 
-button.forEach(option => {
+  button.forEach((option) => {
+    option.addEventListener("click", function getChoices() {
+      // console.log(this.id, this.innerHTML)
+
+      function getPlayerSelection() {
+        if (option.id === "rock") {
+          return (playerSelection = "rock");
+        } else if (option.id === "paper") {
+          return (playerSelection = "paper");
+        } else if (option.id === "scizzors") {
+          return (playerSelection = "scizzors");
+        }
+      }
+
+      getPlayerSelection();
+
+      function getComputerChoice() {
+        return (computerSelection =
+          options[Math.floor(Math.random() * options.length)]);
+      }
+
+      getComputerChoice();
+
+    //   console.log(playerSelection);
+    //   console.log(computerSelection);
+
+      // Result of game() based on PlayerSelection vs. Randomly generated ComputerSelection
     
-   
-    option.addEventListener('click', function getChoices(){
-        // console.log(this.id, this.innerHTML)
 
+      if (playerSelection === computerSelection) {
+        outcomeToHTML = `Tie! You both selected ${playerSelection}`;
+      } else if (
+        (playerSelection === "rock" && computerSelection === "scizzors") ||
+        (playerSelection === "scizzors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+      ) {
+        outcomeToHTML = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore.textContent ++;
 
-            function getPlayerSelection(){
-                if(option.id === "rock"){
-                    return playerSelection = "rock"
-                }
-
-                else if (option.id === "paper"){
-                    return playerSelection = "paper"
-                }
-
-                else if (option.id === "scizzors"){
-                    return playerSelection = "scizzors"
-                }
-                                     
-            
-        };
-
-
-getPlayerSelection();
-        
-    function getComputerChoice (){
-            return computerSelection = options[Math.floor(Math.random()*options.length)]}; 
-        
-        getComputerChoice();
-
+      } else {
+        outcomeToHTML = `Sorry, ${computerSelection} beats ${playerSelection}`;
+        computerScore.textContent ++;
+      }
+      
+    
+      if (parseInt(computerScore.textContent) === 5 || parseInt(playerScore.textContent) === 5)
+      { if (parseInt(playerScore.textContent) === 5){
+        outcomeToHTML = 'Player Wins!'
+      }
+      else{outcomeToHTML = 'Computer Wins.'}
+    //     console.log(parseInt(computerScore.textContent))
+    //    console.log(parseInt(playerScore.textContent))
 
         
+      };
 
-console.log(playerSelection);
-console.log(computerSelection);
+     
 
-
-// Result of game() based on PlayerSelection vs. Randomly generated ComputerSelection
-
-if (playerSelection === computerSelection){
-    outcomeToHTML = `Tie! You both selected ${playerSelection}`}
-
-else if (playerSelection === "rock" && computerSelection === "scizzors" || playerSelection === "scizzors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") 
-{
-    outcomeToHTML = `You win! ${playerSelection} beats ${computerSelection}`}
-
-else {outcomeToHTML = `Sorry, ${computerSelection} beats ${playerSelection}`}
-
-
-// Below to enter match result to HTML
-const outcome = document.querySelector('#outcome');
-outcome.textContent = "Play Again?";
-
-const instructions = document.querySelector('#computerOption');
-instructions.textContent = outcomeToHTML;
+      // Below to enter match result to HTML
 
 
 
-});
+      const outcome = document.querySelector("#outcome");
+      outcome.textContent = "Play Again?";
+
+      const instructions = document.querySelector("#computerOption");
+      instructions.textContent = outcomeToHTML;
+
+    
 
 
+    });
+  });
+}
 
-});
-
-
-
-};
-
-
-
-game();
+playRound();
 
 
